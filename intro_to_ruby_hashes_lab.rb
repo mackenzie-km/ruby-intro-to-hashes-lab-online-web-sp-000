58aa305108924770405ee6e039759ef2999eba69
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 23:07:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,11 @@ def actor
 actor = {:name => "Dwayne The Rock Johnson"}
 end
 
-require "pry"
+
 def monopoly
 	monopoly = {}
   monopoly["railroads"] = "no"
 end
-  binding.pry
 def monopoly_with_second_tier
 	
 end
