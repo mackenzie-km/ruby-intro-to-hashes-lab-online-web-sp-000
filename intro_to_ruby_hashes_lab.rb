ac4820b2770ab7f966b71e57005d5e56e3360319
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 23:35:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,11 @@ end
 
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {}}
-	monopoly[:railroads] = {:pieces => 4}
-	monopoly[:names] = {}
+	monopoly = {
+	  :railroads => {},
+	  
+	}
+
 end
 
 def monopoly_with_third_tier
