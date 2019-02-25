f195bfffd3b361ca95e5fd6783ab7b29d7fb0379
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 23:41:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,7 +50,9 @@ def monopoly_with_fourth_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names =>{
-	      :reading_railroad => {},
+	      :reading_railroad => {
+	        "mortage"
+	      },
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {}, 
 	      :shortline => {}
